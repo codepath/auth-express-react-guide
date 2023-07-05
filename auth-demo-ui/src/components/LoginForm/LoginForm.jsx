246021084198536b649b6682bd663645207b33dc
Tim Lee <leet@meta.com>
2023-07-04 23:28:42 -0700

    Fix styling of logged in page

--- auth-demo-ui/src/components/LoginForm/LoginForm.jsx
+++ auth-demo-ui/src/components/LoginForm/LoginForm.jsx
@@ -41,8 +41,7 @@ const LoginForm = () => {
   };
 
   return (
-    <div>
-      {/* <h2>Login</h2> */}
+    <div className='login-form-container'>
       <form className="login-form" onSubmit={handleLogin}>
         <h2>Login</h2>
         <div className="form-group">
@@ -70,7 +69,6 @@ const LoginForm = () => {
           New to the app? <Link to="/signup">Sign Up</Link>
         </p>
       </form>
-      {/* <p>New to the app? <Link to="/signup">Sign Up</Link></p> */}
     </div>
   );
 };
--- auth-demo-ui/src/components/SignupForm/SignupForm.jsx
+++ auth-demo-ui/src/components/SignupForm/SignupForm.jsx
@@ -53,19 +53,9 @@ const SignupForm = () => {
   };
 
   return (
-    <div>
+    <div className="signup-form-container">
       <form className="signup-form" onSubmit={handleSubmit}>
         <h2>Sign Up</h2>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
         <div className="form-group">
           <label htmlFor="username">Username:</label>
           <input
